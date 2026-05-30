81d8d84d0b3d9e39af95746504edc99b75769f98
Elias <eliasstalfreelancer@gmail.com>
2026-05-30 02:16:27 +0200

    addded mean to graph metrics for less noice on graph

--- ai/viz.py
+++ ai/viz.py
@@ -2,26 +2,32 @@ import matplotlib.pyplot as plt
 class Viz:
     def __init__(self):
         self.loss_history = []
+        self.mean_loss_history = []
 
         self.score_history = []
+        self.mean_score_history = []
 
         self.epsilon_history = []
 
         plt.ion()
-        
+
     def update_graph(self):
 
         plt.clf()
-
         # LOSS
         plt.subplot(3,1,1)
-        plt.plot(self.loss_history)
-        plt.title("Loss")
+        avg_loss = sum(self.loss_history[-100:]) / 100
+        self.mean_loss_history.append(avg_loss)
+        plt.plot(self.mean_loss_history)
+        plt.title("mean loss")
 
         # SCORE
         plt.subplot(3,1,2)
-        plt.plot(self.score_history)
-        plt.title("Score")
+        
+        avg_score = sum(self.score_history[-100:]) / 100
+        self.mean_score_history.append(avg_score)
+        plt.plot(self.mean_score_history)
+        plt.title("mean score")
 
         # EPSILON
         plt.subplot(3,1,3)
